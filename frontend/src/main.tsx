540869e6cc696c109cd6fa26b743f04b26dd3845
Joshua Harnett <joshdot@gmail.com>
2026-01-06 14:47:14 -0800

    switched some code to accomodate front end code

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import GameSearchPrototype from "./components/GameSearchPrototype";
+import GameSearch from "./components/GameSearch";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <GameSearchPrototype />
+    <GameSearch />
   </React.StrictMode>
 );
